1254421b176709e33a43b1ccaf777c711559d875
sam0rr <samor140@hotmail.com>
2025-11-23 02:09:22 -0500

    feat : FLUTTER_ROOT

--- config.fish
+++ config.fish
@@ -3,3 +3,8 @@ on_start
 #Init Zoxide (z)
 zoxide init fish | source
 mise activate fish | source
+
+# Auto-set FLUTTER_ROOT based on whichever flutter is active
+if type -q flutter
+    set -gx FLUTTER_ROOT (dirname (dirname (realpath (command -s flutter))))
+end
