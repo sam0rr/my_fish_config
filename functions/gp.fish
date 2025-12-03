d829183bf67d1f51df870e92713c82ab259e0769
sam0rr <samor140@hotmail.com>
2025-12-03 01:38:51 -0500

    feat : trash alias

--- functions/gp.fish
+++
@@ -1,8 +0,0 @@
-function gp --wraps gtrash --description 'Send files to trash via gtrash put'
-    if not command -q gtrash
-        printf '%s\n' 'gtrash is not available; install it or use "\rm" for permanent deletion.' >&2
-        return 127
-    end
-
-    command gtrash put $argv
-end
---
+++ functions/trash.fish
@@ -0,0 +1,3 @@
+function trash --wraps=gtrash --description 'alias trash=gtrash'
+    gtrash $argv
+end
