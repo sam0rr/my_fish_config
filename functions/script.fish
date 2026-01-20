a541132a851ef860c9e8eba2d8fdf0e944b33851
sam0rr <samor140@hotmail.com>
2026-01-20 11:10:09 -0500

    feat : add script func to make script quick

---
+++ functions/script.fish
@@ -0,0 +1,21 @@
+function script --description 'Create a new executable bash script'
+    if test (count $argv) -eq 0
+        echo ""
+        echo "Usage: script <name>"
+        echo ""
+        return 1
+    end
+
+    set -l filename "$argv[1].sh"
+
+    if test -e $filename
+        echo "Error: File '$filename' already exists."
+        return 1
+    end
+
+    echo "#!/usr/bin/env bash" > $filename
+    chmod +x $filename
+    echo ""
+    echo "Created executable script: $filename"
+    echo ""
+end
